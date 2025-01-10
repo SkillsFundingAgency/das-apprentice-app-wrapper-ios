78b03c9e731b79933bfcc6a5936ebb4cd4f3bfe4
Furqan Agwan <19633375+furqanagwan@users.noreply.github.com>
2025-01-10 16:47:33 +0000

    Transfer work over

---
+++ src/MyApprenticeship/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace myapprenticeship;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/MyApprenticeship/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+namespace myapprenticeship;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
